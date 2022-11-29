5a03c6069ba59aa2feee22e24e7a8665b00b17cb
tmunkhb <tmunkhb@gmail.com>
2022-11-29 18:13:23 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-API',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
